aee3ebdcc30190c68a1dfbc8fc509919641b23ec
Moshe Holtzberg <moholtzberg@gmail.com>
2018-02-23 13:36:29 -0500

    add state and terms and fix sales_rep_id param in allowed params

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -106,6 +106,7 @@ class OrdersController < ApplicationController
   
   def create
     authorize! :create, Order
+    params[:order][:terms] = "terms" unless params[:order][:terms].nil? 
   end
 
   def destroy
@@ -187,7 +188,7 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    temp_params = params.require(:order).permit(:account_name, :sales_re_id, :sales_rep_name, :number, :email, :po_number, 
+    temp_params = params.require(:order).permit(:account_name, :state, :terms, :sales_rep_id, :sales_rep_name, :number, :email, :po_number, 
       :submitted_at, :notes, :credit_hold, :shipping_method, :shipping_amount, :tax_rate, :tax_amount, 
       :bill_to_account_name, :bill_to_attention, :bill_to_address_1, :bill_to_address_2, :bill_to_city, 
       :bill_to_state, :bill_to_zip, :bill_to_phone, :bill_to_email, :ship_to_account_name, :ship_to_attention, 
